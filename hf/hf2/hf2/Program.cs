85c5431ab7a8803b2b4825e2ae516d0ba1f6635a
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2023-09-11 10:55:44 +0200

    hf2

---
+++ hf/hf2/hf2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 100; i += 5)
+                Console.Write(i);
+            Console.WriteLine();
+            for (int i = 10; i < 100; i++)
+                if (i % 5 == 0)
+                    Console.Write(i);
+            Console.WriteLine();
+            for (int i = 0; i < 100; i++)
+                if (i >= 10 && i % 5 == 0)
+                    Console.Write(i);
+            Console.WriteLine();
+        }
+    }
+}
