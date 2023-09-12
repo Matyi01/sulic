ab4beca64b7a086ffdd5d1d20801350adfa9d4ae
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2023-09-12 21:35:52 +0200

    hf3

---
+++ hf/hf3/hf3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i <= 10; i++)
+            {
+                double sqr = Math.Pow(i, 2);
+                Console.WriteLine(sqr);
+            }
+        }
+    }
+}
