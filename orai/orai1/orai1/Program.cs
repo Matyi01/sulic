569a315783bb2ee1efd87ee36d3243f5e47e5a7e
Mátyás Holczer <holczer.matyas.21@ady-nagyatad.hu>
2023-09-13 12:35:43 +0200

    orai1

---
+++ orai/orai1/orai1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+Console.Write("Kérek egy egész számot 0 és 999999 között: ");
+int szam = Convert.ToInt32(Console.ReadLine());
+
+if (szam >= 0 && szam <= 999999)
+{
+    for (int i = 1000000; i >= 0; i /= 10)
+    {
+        Console.WriteLine("A szám"+i+"számjegyű");
+    }
+}
+else
+{
+    Console.Write("Ez a szám nem helyes, kérek egy egész számot 0 és 999999 között: ");
+    szam = Convert.ToInt32(Console.ReadLine());
+}
+
+
+/*
+while (szam >= 0 && szam <= 999999)
+{
+    Console.WriteLine(Math.Floor(Math.Log10(szam) + 1));
+    Console.Write("Kérek egy egész számot 0 és 999999 között: ");
+    szam = Convert.ToInt32(Console.ReadLine());
+}
+*/
