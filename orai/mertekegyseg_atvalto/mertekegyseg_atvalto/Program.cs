8461ebd13b373a2aacff023bd8cf5bea71a9bc02
Mátyás Holczer <holczer.matyas.21@ady-nagyatad.hu>
2023-10-03 09:37:50 +0200

    mertekegyseg

---
+++ orai/mertekegyseg_atvalto/mertekegyseg_atvalto/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Reflection.Metadata;
+
+bool jo = false;
+
+while (!jo)
+{
+    jo = true;
+    Console.Write("Mit váltsak át? (pl.: 12 kg) ");
+    string be = Console.ReadLine();
+
+    string[] darab = be.Split(" ");
+    if (darab.Length != 2)
+    {
+        jo = false;
+        continue;
+    }
+
+    double szam = 0;
+    try
+    {
+        szam = Convert.ToDouble(darab[0]);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nem jó formátum! Legyen ilyen: 12,2 kg");
+        jo = false;
+        continue;
+    }
+
+    Console.WriteLine(szam);
+}
+
+
+
