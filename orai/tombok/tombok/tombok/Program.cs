6ec765b0a51d11f0da31a3b24f0568b53cef0013
Mátyás Holczer <holczer.matyas.21@ady-nagyatad.hu>
2023-10-10 14:04:02 +0200

    asd

---
+++ orai/tombok/tombok/tombok/Program.cs
@@ -0,0 +1,28 @@
+﻿void kiir(int[] tomb)
+{
+    foreach (int i in tomb)
+    {
+        Console.WriteLine("{0}. szam", i);
+    }
+}
+
+int[] szamok = new int[100];
+
+for (int i = 0; i < szamok.Length; i++)
+{
+    szamok[i] = i+1;
+}
+
+Console.WriteLine(szamok);
+foreach (int i in szamok)
+{
+    Console.WriteLine("{0}. szam",i);
+}
+Console.WriteLine();
+
+for (int i = 0;i < szamok.Length; i++)
+{
+    szamok[i] = szamok[i] * 2;
+}
+
+kiir(szamok);
