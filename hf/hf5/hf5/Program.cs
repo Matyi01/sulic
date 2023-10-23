d4954c35cdcd83056f97e32ea89998e3eba2b41e
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2023-10-23 18:16:42 +0200

    hf5

---
+++ hf/hf5/hf5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Kérek egy több szavas szöveget: ");
+string szo = Console.ReadLine();
+
+string[] kiir = szo.Split(" ");
+for  (int i = 0; i < kiir.Length; i++)
+{
+    if (kiir[i] != "")
+    {
+        Console.Write(kiir[i].Substring(0, 1).ToUpper() + kiir[i].Substring(1).ToLower() + " ");
+    }
+}
