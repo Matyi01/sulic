3143a006b3a3d1a89c13498a0458f944fb24d441
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-10-24 12:34:51 +0200

    string2

---
+++ orai/stringek/string2/string2/Program.cs
@@ -0,0 +1,28 @@
+﻿string szo = "";
+string betuk = "qwertzuiopasdfghjklyxcvbnm-";
+
+while (szo == "")
+{
+    Console.Write("Kérek egy szót: ");
+    szo = Console.ReadLine();
+    for (int i = 0; i < szo.Length; i++)
+    {
+        bool temp = false;
+        for (int j = 0; j < betuk.Length; j++)
+        {
+            if (szo[i].ToString().ToLower() == betuk[j].ToString())
+            {
+                temp = true;
+                break;
+            }
+        }
+        if (!temp)
+        {
+            Console.WriteLine("Ez nem egy szó");
+            szo = "";
+        }
+    }
+}
+Console.WriteLine(szo);
+
+
