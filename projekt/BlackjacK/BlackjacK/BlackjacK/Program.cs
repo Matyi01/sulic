4a0ccbd96098b87e1e51143473efc6bf391f0af3
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-11-15 12:33:52 +0100

    asd

---
+++ projekt/BlackjacK/BlackjacK/BlackjacK/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> kartyak = new List<string>();
+List<string> szinek = new List<string>() {"Treff", "Pikk", "Káró", "Kőr"};
+List<string> szamok = new List<string>() {"2", "3", "4", "5", "6", "7", "8", "9", "10", "jumbo", "dáma", "király", "ász"};
+Random rand = new Random();
+for (int j = 0; j < szinek.Count; j++)
+{
+    for (int k = 0; k < szamok.Count; k++)
+    {
+        kartyak.Add($"{szinek[j]} {szamok[k]}");
+    }
+}
+for (int i = 0; i < kartyak.Count; i++)
+{
+    Console.WriteLine(kartyak[i]);
+}
+
+
+/*
+string randszin = szinek[rand.Next(szinek.Count)];
+string randszam = szamok[rand.Next(szamok.Count)];
+*/
