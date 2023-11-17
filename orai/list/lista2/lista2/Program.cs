17b91270da554903f77cc02431c7c7864f209d6e
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-11-17 08:45:14 +0100

    asd

---
+++ orai/list/lista2/lista2/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> szamok = new List<int>();
+Random rand = new Random();
+int darab = rand.Next(10000, 100000);
+for (int i = 0; i < darab; i++)
+{
+    szamok.Add(rand.Next(10,100));
+}
+
+Console.WriteLine($"{szamok.Count()} darab szám van");
+
+Dictionary<int, int> darabszam = new Dictionary<int, int>();
+for (int i = 0;i < darab; i++)
+{
+    if (darabszam.ContainsKey(i))
+    {
+        darabszam[i]++;
+    }
+    else
+    {
+        darabszam.Add(i, 0);
+    }
+}
+foreach (KeyValuePair<int, int> kvp in darabszam)
+{
+    Console.WriteLine(kvp.Key, kvp.Value);
+}
