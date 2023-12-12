99d407b1c5453b4963eab1ec79c4a4c261f16fa7
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2023-12-12 14:05:50 +0100

    asd

---
+++ orai/class1/class1/Program.cs
@@ -0,0 +1,44 @@
+﻿class auto
+{
+    public int kerekszam = 4;
+
+    public void dudal()
+    {
+        Console.WriteLine("tütü");
+    }
+
+
+    private string _tulaj;
+    public string tulajdonos
+    {
+        get
+        {
+            return _tulaj;
+        }
+        set
+        {
+            if (value == "Bence")
+            {
+                Console.WriteLine("Nem!");
+            }
+            else
+            {
+                _tulaj = value;
+            }
+        }
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+        auto kocsi = new auto();
+        Console.WriteLine(kocsi.kerekszam);
+        kocsi.dudal();
+        kocsi.tulajdonos = "Gabi";
+        Console.WriteLine(kocsi.tulajdonos);
+        kocsi.tulajdonos = "Bence";
+        Console.WriteLine(kocsi.tulajdonos);
+    }
+}
