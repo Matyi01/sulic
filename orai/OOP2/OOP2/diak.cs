b05474b1af47d51bfd28a72a8b7d7f89973d45ef
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2024-01-09 14:03:12 +0100

    asd

---
+++ orai/OOP2/OOP2/diak.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class diak : udvariasember
+    {
+        private int kor;
+        public diak(string vnev, string knev, int kor) : base(vnev, knev) 
+        { 
+            this.kor = kor;
+        }
+        public void iskola()
+        {
+            if (kor < 2) { Console.WriteLine("ÓóÓóÁáÁá"); }
+            else if (kor < 3) { Console.WriteLine("Bölcsibe"); }
+            else if (kor < 7) { Console.WriteLine("Ovis"); }
+            else if (kor < 11) { Console.WriteLine("Alsós"); }
+            else if (kor < 15) { Console.WriteLine("Felsős"); }
+            else if (kor < 20) { Console.WriteLine("Technikum"); }
+            else if (kor < 25) { Console.WriteLine("Egyetem"); }
+            else { Console.WriteLine("Felsőoktatás"); }
+        }
+        public void koszon(diak diakvaltozo)
+        {
+            Console.WriteLine("Csá {0}", diakvaltozo.keresztnev);
+        }
+    }
+}
---
+++ orai/OOP2/OOP2/udvariasember.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class udvariasember : ember
+    {
+        public udvariasember(string vnev, string knev) : base(vnev, knev)
+        {
+
+        }
+        public void koszon()
+        {
+            Console.WriteLine("Szia uram!");
+        }
+        public void koszon(ember embervaltozonev)
+        {
+            Console.WriteLine("Szia {0} uram!", embervaltozonev.teljesnev);
+        }
+        public void koszonnevvel()
+        {
+            Console.WriteLine("Szia uram! {0} vagyok", teljesnev);
+        }
+        public void koszonnevvel(ember embervaltozonev)
+        {
+            Console.WriteLine("Szia {0} uram! {1} vagyok", embervaltozonev.teljesnev, teljesnev);
+        }
+    }
+}
