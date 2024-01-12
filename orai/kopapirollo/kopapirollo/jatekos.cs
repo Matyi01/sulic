39c786c0ae69e44d46120be40e9229f52847fefc
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2024-01-12 08:45:40 +0100

    asd

---
+++ orai/kopapirollo/kopapirollo/jatekos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kopapirollo
+{
+    internal class jatekos
+    {
+        string[] lehetosegek = new string[] {"kő", "papír", "olló"};
+        public string valasztas;
+        public jatekos()
+        {
+            Random rand = new Random();
+            valasztas = lehetosegek[rand.Next(0,lehetosegek.Length)];
+        }
+        public void kinyer(jatekos masik)
+        {
+            if (valasztas == masik.valasztas)
+            {
+                Console.WriteLine("Döntetlen");
+            }
+            else
+            {
+                if (valasztas == lehetosegek[0] && masik.valasztas == lehetosegek[1])
+                {
+
+                }
+            }
+        }
+    }
+}
