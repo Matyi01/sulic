a14e47d9cdc711baa92be2e202cbcca10d5233bd
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2024-01-16 09:39:41 +0100

    asd

---
+++ orai/cegesautok1/cegesautok1/Program.cs
@@ -0,0 +1,24 @@
+﻿using cegesautok1;
+
+List<adatok> autok = new List<adatok>();
+
+StreamReader olvas = new StreamReader("autok.txt");
+
+while (!olvas.EndOfStream)
+{
+    string sor = olvas.ReadLine();
+    adatok a1 = new adatok(sor);
+    autok.Add(a1);
+}
+
+olvas.Close();
+
+Console.WriteLine("2. feladat");
+for (int i = autok.Count-1; i > 0; i--)
+{
+    if (!autok[i].parkole())
+    {
+        Console.WriteLine("{0}. nap rendszám: {1}", autok[i].nap, autok[i].rendszam);
+        break;
+    }
+}
---
+++ orai/cegesautok1/cegesautok1/adatok.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cegesautok1
+{
+    internal class adatok
+    {
+        public int nap;
+        public string ido;
+        public string rendszam;
+        public int szemelyid;
+        public int km;
+        bool parkol;
+
+        public adatok(string sor)
+        {
+            string[] vag = sor.Split(" ");
+            nap = Convert.ToInt32(vag[0]);
+            ido = vag[1];
+            rendszam = vag[2];
+            szemelyid = Convert.ToInt32(vag[3]);
+            km = Convert.ToInt32(vag[4]);
+            parkol = vag[5] == "1";
+        }
+        public bool parkole()
+        {
+            return parkol;
+        }
+    }
+}
