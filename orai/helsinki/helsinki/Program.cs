70e079102770dbe0f4267cddb4e812a44ba25f67
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2024-01-19 11:28:10 +0100

    asd

---
+++ orai/helsinki/helsinki/Program.cs
@@ -0,0 +1,27 @@
+﻿using helsinki1952;
+
+List<adatok> eredmnenyek = new List<adatok>();
+StreamReader olvas = new StreamReader("helsinki.txt");
+while (!olvas.EndOfStream)
+{
+    eredmnenyek.Add(new adatok(olvas.ReadLine()));
+}
+olvas.Close();
+
+Console.WriteLine("3. feladat:");
+Console.WriteLine("Pontszerző helyezések száma: {0}", eredmnenyek.Count);
+
+int[] pontok = new int[7];
+for (int i = 0; i < eredmnenyek.Count; i++)
+{
+    pontok[eredmnenyek[i].helyezes]++;
+}
+Console.WriteLine("4. feladat");
+Console.WriteLine("Arany: {0}", pontok[1]);
+Console.WriteLine("Ezüst: {0}", pontok[2]);
+Console.WriteLine("Bronz: {0}", pontok[3]);
+Console.WriteLine("Összesen: {0}", pontok[1] + pontok[2] + pontok[3]);
+
+Console.WriteLine("5.fealdat");
+Console.WriteLine("Olimpiai pontok száma: {0}", pontok[1]*7 + pontok[2] * 5 + pontok[3] * 4 + pontok[4] * 3 + pontok[5] * 2 + pontok[6] * 1);
+
---
+++ orai/helsinki/helsinki/adatok.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helsinki1952
+{
+    internal class adatok
+    {
+        public int helyezes;
+        public int fo;
+        public string sportag;
+        public string versenyszam;
+        public adatok(string sor)
+        {
+            string[] vag = sor.Split(" ");
+            helyezes = Convert.ToInt32(vag[0]);
+            fo = Convert.ToInt32(vag[1]);
+            sportag = vag[2];
+            versenyszam = vag[3];
+        }
+    }
+}
