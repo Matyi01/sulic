98dbdf2cc0cdbaa6ba6594f81df6c32bf91a8218
Holczer Mátyás <holczerm21@ADY-NAGYATAD.SULINET.HU>
2024-01-26 11:28:05 +0100

    asd

---
+++ orai/fejvagyiras1/fejvagyiras1/Program.cs
@@ -0,0 +1,14 @@
+﻿using fejvagyiras1;
+
+List<adatok> dobasok = new List<adatok>();
+StreamReader olvas = new StreamReader("kiserlet.txt");
+while (!olvas.EndOfStream)
+{
+    string sor = olvas.ReadLine();
+    if (dobasok[dobasok.Count - 1].dobas == sor)
+    {
+        dobasok[dobasok.Count - 1].db++;
+    }
+    dobasok.Add());
+}
+olvas.Close();
