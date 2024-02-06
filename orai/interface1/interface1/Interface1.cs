0fe135d349786eb02e04fe9ba3634be802523648
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-02-06 14:03:36 +0100

    asd

---
+++ orai/interface1/interface1/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface1
+{
+    internal interface Interface1
+    {
+        int szam();
+        void kiir();
+        int tusko { get; set; }
+    }
+}
---
+++ orai/interface1/interface1/Program.cs
@@ -0,0 +1,10 @@
+﻿using interface1;
+
+Class1 proba = new Class1();
+
+Console.WriteLine(proba.tusko);
+proba.tusko = 100;
+Console.WriteLine(proba.tusko);
+Console.WriteLine(proba.szam());
+
+proba.kiir();
