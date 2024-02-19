b7fdec0f375f1a889faa2ed3d87dd62c400b59ac
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2024-02-19 18:39:10 +0100

    asd

---
+++ hf/wpfhf2/wpfhf2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfhf2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kep1.Opacity = slider1.Value * slider4.Value;
+            kep2.Opacity = slider1.Value * slider2.Value;
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kep3.Opacity = slider3.Value * slider2.Value;
+            kep4.Opacity = slider3.Value * slider4.Value;
+        }
+
+        private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kep3.Opacity = slider2.Value * slider3.Value;
+            kep2.Opacity = slider2.Value * slider1.Value;
+        }
+
+        private void slider4_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kep1.Opacity = slider4.Value * slider1.Value;
+            kep4.Opacity = slider4.Value * slider3.Value;
+        }
+
+
+    }
+}
--- orai/probaWPF/probaWPF/obj/Debug/net6.0-windows/App.g.i.cs
+++ orai/probaWPF/probaWPF/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace probaWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace probaWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public static void Main() {
             probaWPF.App app = new probaWPF.App();
             app.InitializeComponent();
