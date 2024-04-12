5870ae4e96e3a846591517743b28a0861960215b
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-04-12 11:30:35 +0200

    asd

--- orai/tort/tort/MainWindow.xaml.cs
+++ orai/tort/tort/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,90 @@ namespace tort
         public MainWindow()
         {
             InitializeComponent();
+        }
+        /*
+        Függvény lnko(a, b : egész számok) : egész szám
+            ha a=b akkor lnko := a
+            ha a<b akkor lnko := lnko(a, b-a)
+            ha a>b akkor lnko := lnko(a-b, b)
+        Függvény vége
+        */
+        private int lnko(int a, int b)
+        {
+            if (a == b) return a;
+            else if (a < b) return lnko(a, b - a);
+            else if (a > b) return lnko(a - b, b);
+            return 0;
+        }
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int szamlalo = 0;
+            try
+            {
+                szamlalo = Convert.ToInt32(textBox.Text);
+            }
+            catch { }
+
+            int nevezo = 0;
+            try
+            {
+                nevezo = Convert.ToInt32(textBox1.Text);
+            }
+            catch { }
+
+            if (nevezo != 0 && szamlalo % nevezo == 0)
+            {
+                textBlock4.Text = (szamlalo / nevezo).ToString();
+            }
+            else if (nevezo != 0 && szamlalo % nevezo != 0)
+            {
+                textBlock4.Text = "nem egész";
+            }
+            else
+            {
+                textBlock4.Text = "Nullával való osztás";
+            }
+
+            textBlock.Text = szamlalo.ToString();
+            textBlock1.Text = nevezo.ToString();
+
+            if (nevezo != 0 && szamlalo != 0)
+            {
+                int oszto = lnko(szamlalo, nevezo);
+                textBlock2.Text = (szamlalo / oszto).ToString();
+                textBlock3.Text = (nevezo / oszto).ToString();
+
+                tab4.IsEnabled = true;
+            }
+            else
+            {
+                tab4.IsEnabled = false;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int szamlalo = 0;
+            try
+            {
+                szamlalo = Convert.ToInt32(textBox2.Text);
+            }
+            catch { }
+
+            int nevezo = 0;
+            try
+            {
+                nevezo = Convert.ToInt32(textBox3.Text);
+            }
+            catch { }
+
+            textBlock_Copy1.Text = szamlalo.ToString();
+            textBlock1_Copy1.Text = nevezo.ToString();
+        }
+
+        private void tab4_GotFocus(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
