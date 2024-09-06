c295ae4001f242a35290ec54340a5039d263fea2
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-09-06 13:27:22 +0200

    szoveg

---
+++ orai2/szavakkk/szavakkk/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] forras = File.ReadAllLines("szoveg.txt");
+
+List<string> szoveg = new List<string>();
+for (int i = 0; i < forras.Length; i += 2)
+{
+    szoveg.Add(forras[i]);
+}
+
+for(int i = 0; i > szoveg.Count; i++)
+{
+    szoveg[i].Replace("...", "");
+}
