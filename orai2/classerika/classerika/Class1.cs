144e88c48e339cbf02803fa7143c24345040da2c
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-09-16 14:13:56 +0200

    erikaclass

---
+++ orai2/classerika/classerika/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classerika
+{
+    internal class Class1
+    {
+        public int szam;
+        public Random random = new Random();
+        public Class1()
+        {
+            szam = random.Next(-100, 100);
+        }
+        static public bool parose()
+        {
+            if (szam % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ orai2/classerika/classerika/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Készítsetek egy class-t, ami példányosításkor generál egy (int) egész számot, ami lehet negatív is.
+A classnak legyenek függvényei, amik megmondják (logikai értékkel), hogy 
+páros-e
+osztható-e 3-mal
+osztható-e 5-tel
+osztható-e 10-zel
+osztható-e a paraméterben átadott számmal
+Majd próbáljátok ki, és keressetek hozzá teszt eseteket, amivel a helyes működést tudjátok kipróbálni.
+pl. osztható-e ezekkel: 23, 42, 100 000, 3 000 000 000, 2, -2, 1.2, -1.2, 0, -0
+*/
