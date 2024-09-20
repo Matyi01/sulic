2513246f3987451058f6b303f2d4f6d7f1e5ccc5
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-09-20 09:39:09 +0200

    idojaras

---
+++ orai2/idojaras/idojaras/Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+StreamWriter ir = new StreamWriter("ido.txt");
+
+int fok = random.Next(10,20);
+int ora = 0;
+for (int i = 0; i < 14; i++)
+{
+    while(ora % 24 < 6 || ora % 24 > 18)
+    {
+        ora++;
+        fok = random.Next(fok - 5, fok);
+        if (fok < 10)
+        {
+            fok = 10;
+        }
+        ir.WriteLine(fok + " C° nap: " + ora / 24 + " óra: " + ora % 24);
+    }
+    while (ora % 24 <= 18 && ora % 24 >= 6)
+    {
+        ora++;
+        fok = random.Next(fok, fok + 5);
+        if (fok > 40)
+        {
+            fok = 40;
+        }
+        ir.WriteLine(fok + " C° nap: " + ora / 24 + " óra: " + ora % 24);
+    }
+}
+ir.Close();
