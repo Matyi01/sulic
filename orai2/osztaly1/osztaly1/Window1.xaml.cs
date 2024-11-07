01605120080315c731f0ca92f48015439f0dd482
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-11-07 08:45:17 +0100

    asd

---
+++ orai2/osztaly1/osztaly1/Window1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace osztaly1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private int megnyitasDB = 0;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public void show()
+        {
+
+        }
+    }
+}
--- orai2/osztaly1/osztaly1/obj/Debug/net8.0-windows/osztaly1.AssemblyInfo.cs
+++ orai2/osztaly1/osztaly1/obj/Debug/net8.0-windows/osztaly1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("lotto")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("osztaly1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69554ed756d93f548ee41dc50d94cf6acd82542")]
-[assembly: System.Reflection.AssemblyProductAttribute("lotto")]
-[assembly: System.Reflection.AssemblyTitleAttribute("lotto")]
+[assembly: System.Reflection.AssemblyProductAttribute("osztaly1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("osztaly1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
