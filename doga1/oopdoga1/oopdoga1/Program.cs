6a674c7cde18ef524437a5b84d88e40ae0f4e183
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2024-11-28 10:45:11 +0100

    asd

---
+++ doga1/oopdoga1/oopdoga1/Program.cs
@@ -0,0 +1,29 @@
+﻿using oopdoga1;
+
+penztarca Jurgen = new penztarca(1700);
+penztarca Frau = new penztarca(900);
+penztarca fizetes = new penztarca(400);
+penztarca tomott = new penztarca(400);
+int ajandekok = 800;
+int adossag = 5500;
+
+Console.WriteLine(Jurgen);
+
+Jurgen -= 100; //fia zsebpénze | class - int
+Console.WriteLine(Jurgen);
+
+Jurgen -= Frau; //frau fizet | class - class
+Console.WriteLine(Jurgen);
+
+Jurgen += 400; //kp fizetés | class + int
+Console.WriteLine(Jurgen);
+
+Jurgen += fizetes; //pénztárcás fizetés | class + class
+Console.WriteLine(Jurgen);
+Console.WriteLine(fizetes);
+
+ajandekok += tomott; //lakodalomban ajándék | int + class
+Console.WriteLine(ajandekok);
+
+adossag -= Jurgen; //adósság csökkentése | int - class
+Console.WriteLine(adossag);
---
+++ doga1/oopdoga1/oopdoga1/penztarca.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopdoga1
+{
+    internal class penztarca
+    {
+        public int penz;
+        public penztarca(int penz)
+        {
+            this.penz = penz;
+        }
+        public static penztarca operator -(penztarca p1, int p2)
+        {
+            penztarca tempp1 = new penztarca(p1.penz);
+
+            if (tempp1.penz >= p2)
+            {
+                tempp1.penz -= p2;
+                return tempp1;
+            }
+            else
+            {
+                return tempp1;
+            }
+        }
+        public static penztarca operator -(penztarca p1, penztarca p2)
+        {
+            penztarca tempp1 = new penztarca(p1.penz);
+            penztarca tempp2 = new penztarca(p2.penz);
+
+            if (tempp1.penz >= tempp2.penz)
+            {
+                tempp1.penz -= tempp2.penz;
+                tempp2.penz += tempp1.penz;
+                return tempp1;
+            }
+            else
+            {
+                return tempp1;
+            }
+        }
+        public static int operator -(int p1, penztarca p2)
+        {
+            penztarca tempp2 = new penztarca(p2.penz);
+
+            if (p1 >= tempp2.penz)
+            {
+                p1 -= tempp2.penz;
+                tempp2.penz += p1;
+                return p1;
+            }
+            else
+            {
+                return p1;
+            }
+        }
+        public static penztarca operator +(penztarca p1, int p2)
+        {
+            penztarca tempp1 = new penztarca(p1.penz);
+            return tempp1;
+        }
+        public static penztarca operator +(penztarca p1, penztarca p2)
+        {
+            penztarca tempp1 = new penztarca(p1.penz);
+            penztarca tempp2 = new penztarca(p2.penz);
+
+
+                tempp1.penz += tempp2.penz;
+                tempp2.penz -= tempp1.penz;
+                return tempp1;
+
+        }
+        public static int operator +(int p1, penztarca p2)
+        {
+            penztarca tempp2 = new penztarca(p2.penz);
+
+
+                p1 += tempp2.penz;
+                tempp2.penz -= p1;
+                return p1;
+
+        }
+        public override string ToString()
+        {
+            return $"Pénz: {penz} Eur";
+        }
+    }
+}
