5298c6ea1836cb5b1fa857308aae1d669902a26a
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2025-01-10 14:20:11 +0100

    asd

---
+++ orai2/belepteto/belepteto/Program.cs
@@ -0,0 +1,40 @@
+﻿using belepteto;
+
+string[] sorok = File.ReadAllLines("bedat.txt");
+List<adatok> esemenyek = new List<adatok>();
+foreach (string sor in sorok)
+{
+    esemenyek.Add(new adatok(sor));
+}
+
+Console.WriteLine(esemenyek[0].ido);
+Console.WriteLine(esemenyek[esemenyek.Count - 1].ido);
+
+StreamWriter ir = new StreamWriter("kesok.txt");
+foreach (adatok e in esemenyek)
+{
+    if ((e.ora == 7 && e.perc >= 50) || (e.ora == 8 && e.perc <= 15) && e.esemeny == 1){
+        ir.WriteLine(e.ido+" "+e.kod);
+    }
+}
+ir.Close();
+
+int db = 0;
+foreach (adatok e in esemenyek)
+{
+    if (e.esemeny == 3)
+    {
+        db++;
+    }
+}
+Console.WriteLine(db);
+
+HashSet<string> db2 = new HashSet<string>();
+foreach (adatok e in esemenyek)
+{
+    if (e.esemeny == 4)
+    {
+        db2.Add(e.kod);
+    }
+}
+Console.WriteLine(db2.Count);
---
+++ orai2/belepteto/belepteto/adatok.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace belepteto
+{
+    internal class adatok
+    {
+        public string kod;
+        public string ido;
+        public int esemeny;
+        public int ora;
+        public int perc;
+        public adatok(string sor)
+        {
+            string[] vag = sor.Split(" ");
+            kod = vag[0];
+            ido = vag[1];
+            esemeny = Convert.ToInt32(vag[2]);
+
+            ora = Convert.ToInt32(ido.Split(":")[0]);
+            perc = Convert.ToInt32(ido.Split(":")[1]);
+        }
+    }
+}
