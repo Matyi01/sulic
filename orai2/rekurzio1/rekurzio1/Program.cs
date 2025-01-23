047aa385e174efd84ed4f1ac54ac1adfbd3deb13
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2025-01-23 11:30:26 +0100

    asd

--- orai2/rekurzio1/rekurzio1/Program.cs
+++ orai2/rekurzio1/rekurzio1/Program.cs
@@ -19,7 +19,7 @@ static BigInteger fakt2(BigInteger szam, BigInteger elozo)
     BigInteger eredmeny = 1;
     if (szam == 1)
     {
-        return 1;
+        return elozo;
     }
     else if (szam > 1)
     {
@@ -28,6 +28,100 @@ static BigInteger fakt2(BigInteger szam, BigInteger elozo)
     return eredmeny;
 }
 
+static BigInteger fib(BigInteger n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+static int ack(int n, int m)
+{
+    switch (m)
+    {
+        case 0: return m + 1;
+        default:
+            if (n == 0 && m == 0)
+            {
+                return ack(n-1, 1);
+            }
+            else
+            {
+                return ack(n - 1, ack(n, m - 1));
+            }
+    }
+}
+
+static void quick(int[] a, int e, int v)
+{
+    int k = 0;
+    szetvalogat(a, e, v, k);
+    if (k - e > 1)
+    {
+        quick(a, e, k - 1);
+    }
+    if (k - e < 1)
+    {
+        quick(a, k + 1, v);
+    }
+}
+
+static void szetvalogat(int[] a, int e, int v, int k)
+{
+    int x;
+    int l;
+    k = e;
+    l = v;
+    x = a[k];
+    while (k < l)
+    {
+        while (k < l && a[l] > x)
+        {
+            l--;
+        }
+        if (k < l)
+        {
+            a[k] = a[l];
+            k++;
+            while (k < l && a[k] <= x)
+            {
+                k++;
+            }
+            if (k < l)
+            {
+                a[l] = a[k];
+                l--;
+            }
+        }
+    }
+    a[k] = x;
+}
+
 Console.WriteLine(fakt(5));
 Console.WriteLine(fakt(3000));
-Console.WriteLine(fakt2(5, 0));
+Console.WriteLine(fakt2(5, 1));
+Console.WriteLine(fib(10));
+for (int i = 0; i < 20; i++)
+{
+    Console.Write(fib(i) + " ");
+}
+for (int i = 0; i < 20; i++)
+{
+    Console.Write(fib(19 - i) + " ");
+}
+Console.WriteLine();
+Console.WriteLine(ack(4, 0));
+
+int[] szamok = [7, 6, 3, 9, 5, 2, 1, 8, 4];
+Console.WriteLine(szamok);
+quick(szamok, 0, szamok.Length - 1);
+Console.WriteLine(szamok);
---
+++ orai2/roxfort1/roxfort1/Program.cs
@@ -0,0 +1,70 @@
+﻿static double atlag(List<int> e)
+{
+    return e.Sum() / e.Count;
+}
+static int legjobb(List<int> e)
+{
+    return e.Max();
+}
+static int legrosszabb(List<int> e)
+{
+    return e.Min();
+}
+static int osztalyzat(int e)
+{
+    if (e < 40)
+    {
+        return 1;
+    }
+    else if (e < 55)
+    {
+        return 2;
+    }
+    else if (e < 70)
+    {
+        return 3;
+    }
+    else if (e < 85)
+    {
+        return 4;
+    }
+    else
+    {
+        return 5;
+    }
+}
+
+string[] txt = File.ReadAllLines("atvaltoztatastan.txt");
+
+List<int> eredmenyek = new List<int>();
+foreach (string line in txt)
+{
+    eredmenyek.Add(int.Parse(line));
+}
+
+Console.WriteLine(atlag(eredmenyek));
+Console.WriteLine(legjobb(eredmenyek));
+Console.WriteLine(legrosszabb(eredmenyek));
+
+List<int> osztalyzatok = new List<int>();
+foreach (int e in eredmenyek)
+{
+    osztalyzatok.Add(osztalyzat(e));
+}
+
+Dictionary<int, int> db = new Dictionary<int, int>();
+foreach (int e in osztalyzatok)
+{
+    if (db.ContainsKey(e))
+    {
+        db[e]++;
+    }
+    else
+    {
+        db.Add(e, 1);
+    }
+}
+foreach (KeyValuePair<int, int> e in db)
+{
+    Console.WriteLine(e.Key + ": " + e.Value);
+}
