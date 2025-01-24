aa18b363a1845dbe33cf46332d41bec9201e574e
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2025-01-24 14:18:58 +0100

    asd

---
+++ orai2/rekurzio2/rekurzio2/Program.cs
@@ -0,0 +1,13 @@
+﻿static string fordit(string s)
+{
+    if (s.Length == 0)
+    {
+        return s;
+    }
+
+    return fordit(s.Substring(1)) + s[0];
+}
+
+string text = "abcd, efgh";
+
+Console.WriteLine(fordit(text));
