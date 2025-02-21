b92feb923b6efcf4d17238392bd641cffc4cab91
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2025-02-21 14:11:03 +0100

    asd

---
+++ orai2/wpfosszead/wpfosszead/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfosszead
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Convert.ToString(int.Parse(textBox.Text) + int.Parse(textBox1.Text));
+        }
+    }
+}
