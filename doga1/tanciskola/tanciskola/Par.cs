da0efc8b5d4e19baa8ac6a3f971645f1bf0b04cb
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2025-02-21 13:29:47 +0100

    asd

---
+++ doga1/tanciskola/tanciskola/Par.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+namespace tanciskola
+{
+    internal class Par
+    {
+        public string tanc;
+        public string lany;
+        public string fiu;
+        public Par(string tanc, string lany, string fiu)
+        {
+            this.tanc = tanc;
+            this.lany = lany;
+            this.fiu = fiu;
+        }
+        public override string ToString()
+        {
+            return tanc + ": " + lany + ", " + fiu;
+        }
+    }
+}
---
+++ doga1/tanciskola/tanciskola/Program.cs
@@ -0,0 +1,45 @@
+﻿using tanciskola;
+
+List<Par> parok = new List<Par>();
+
+StreamReader olvas = new StreamReader("tancrend.txt");
+
+while (!olvas.EndOfStream)
+{
+    parok.Add(new Par(olvas.ReadLine(), olvas.ReadLine(), olvas.ReadLine()));
+}
+
+olvas.Close();
+
+Console.WriteLine("2. feladat:");
+Console.WriteLine("Az elsőként bemutatott tánc neve: {0}, az utolsóként: {1}", parok.First().tanc, parok.Last().tanc);
+
+Console.WriteLine("3. feladat:");
+Console.WriteLine("A sambát {0} pár muatta be.", parok.Where(e => e.tanc == "samba").Count());
+
+Console.WriteLine("4. feladat:");
+Console.WriteLine("Vilma ezekben a táncokban szerepelt: {0}", string.Join(", ", parok.Where(e => e.lany == "Vilma").Select(e => e.tanc)));
+
+Console.WriteLine("5. feladat:");
+Console.Write("Kérem egy tánc nevét: ");
+string tancbe = Console.ReadLine();
+if (parok.Where(e => e.lany == "Vilma" && e.tanc == tancbe).Count() != 0)
+{
+    Console.WriteLine("A {0} bemutatóján Vilma párja {1} volt.", tancbe, parok.Where(e => e.lany == "Vilma" && e.tanc == tancbe).Select(e => e.fiu).ToArray()[0]);
+}
+else
+{
+    Console.WriteLine("Vilma nem táncolt {0}-t.", tancbe);
+}
+
+
+StreamWriter ir = new StreamWriter("szereplok.txt");
+ir.WriteLine("Lányok: {0}", string.Join(", ", parok.Select(e => e.lany).Distinct()));
+ir.WriteLine("Fiúk: {0}", string.Join(", ", parok.Select(e => e.fiu).Distinct()));
+ir.Close();
+
+
+Console.WriteLine("7. feladat:");
+var fiucsoportok = parok.GroupBy(e => e.fiu).Select(csoport => new { nev = csoport.Key, db = csoport.Count() });
+var lanycsoportok = parok.GroupBy(e => e.lany).Select(csoport => new { nev = csoport.Key, db = csoport.Count() });
+Console.WriteLine("A fiúk közül a legtöbbször szerepelt: {0}, a lányok közül a legtöbbször szerepelt: {1}", string.Join(", ", fiucsoportok.Where(e => e.db == fiucsoportok.Select(e => e.db).ToArray().Max()).Select(e => e.nev)), string.Join(", ", lanycsoportok.Where(e => e.db == lanycsoportok.Select(e => e.db).ToArray().Max()).Select(e => e.nev)));
