8f00c6d672ec249557c0ccabca3358556248b54a
Holczer Mátyás <holczerm21@ady-nagyatad.sulinet.hu>
2025-02-24 14:02:58 +0100

    asd

---
+++ orai2/wpfjatek/wpfjatek/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfjatek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            button.Margin = new Thickness(rnd.Next(1, (int)this.Width - 75), rnd.Next(1, (int)this.Height - 75), 0, 0);
+        }
+        private void button_MouseEnter1(object sender, MouseEventArgs e)
+        {
+            SetCursorPos(rnd.Next(1, (int)this.Width), rnd.Next(1, (int)this.Height));
+        }
+    }
+}
